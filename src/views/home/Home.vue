32203f76ea0e7fa52f61a9e7a00199d3afd90aac
DamonMok <damonmok1216@gmail.com>
2021-03-28 17:07:46 +0800

    完成退出功能

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    主页
+    <el-button type='info' @click="didClickedLogout">退出</el-button>
   </div>
 </template>
 
 <script>
   export default {
-    
+    methods: {
+      // 退出登录
+      didClickedLogout() {
+        // 清空token
+        window.sessionStorage.clear()
+        // 跳转到登录页
+        this.$router.push({name: 'Login'})
+      }
+    },
   }
 </script>
 
