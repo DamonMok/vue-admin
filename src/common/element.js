65cefbd9f7289580140708cc5b03b2a3ce9fce17
DamonMok <damonmok1216@gmail.com>
2021-03-28 17:19:05 +0800

    完成登录功能

--- src/common/element.js
+++ src/common/element.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
-import { Input, Form, FormItem, Button, Message } from 'element-ui';
+import { Input, Form, FormItem, Button, Message, Container, Header, Aside, Main} from 'element-ui';
 
 Vue.use(Input)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 
 Vue.prototype.$message = Message
