fe64d604fea0374f30cd54853dd7ced64c7d7f55
DamonMok <damonmok1216@gmail.com>
2021-04-07 16:18:35 +0800

    渲染权限列表视图

--- src/views/rights/Rights.vue
+++ src/views/rights/Rights.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-    权限列表
+    <!-- 面包屑导航 -->
+    <nav-titles :titles="['权限管理', '权限列表']"></nav-titles>
+
+    <!-- 卡片视图区域 -->
+    <my-card>
+
+      <!-- 表格 -->
+      <el-table :data="rights" border style="width: 100%">
+        <el-table-column type="index" label="#" width="180"></el-table-column>
+        <el-table-column prop="authName" label="权限名称" width="180"></el-table-column>
+        <el-table-column prop="path" label="路径" width="180"></el-table-column>
+        <el-table-column prop="level" label="权限等级"></el-table-column>
+      </el-table>
+    </my-card>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import NavTitles from "components/content/NavTitles";
+import MyCard from "components/content/MyCard";
+
+export default {
+  components: {
+    NavTitles,
+    MyCard,
+  },
+  data() {
+    return {
+      rights: [], // 权限表格数据
+    };
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
