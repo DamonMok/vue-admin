e1960cf8f0c7ede400be0dfcc2939b2e393c051d
DamonMok <damonmok1216@gmail.com>
2021-04-09 00:27:57 +0800

    商品列表-添加分页

--- src/views/goods/Category.vue
+++ src/views/goods/Category.vue
@@ -31,6 +31,10 @@
           <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
         </template>
       </tree-table>
+
+      <!-- 分页 -->
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[1, 2, 5, 10]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total">
+      </el-pagination>
     </my-card>
   </div>
 </template>
@@ -39,52 +43,65 @@
 import NavTitles from "components/content/NavTitles";
 import MyCard from "components/content/MyCard";
 
-import {requestGoodsCategories} from 'network/goods'
+import { requestGoodsCategories } from "network/goods";
 
-  export default {
-    components: {
-      NavTitles,
-      MyCard,
-    },
-    data() {
-      return {
-        queryInfo: {
-          type: [],  // 获取分类的级别
-          pagenum: 1,  // 第几页
-          pagesize: 5  // 每页多少条
-        },
-        categories: [],  // 商品分类列表
-        total: 0,  // 商品总条数
-        columns: [
-          // 分类名称列
-          {label: '分类名称', prop: 'cat_name'},
-          // 是否有效列
-          {label: '是否有效', type: 'template', template: 'isOk'},
-          // 排序列
-          {label: '排序', type: 'template', template: 'level'},
-          // 操作列
-          {label: '操作', type: 'template', template: 'operation'}
-        ]
-      }
-    },
-    methods: {
-      // 获取商品分类列表
-      async getCategories() {
-        const {data: res} = await requestGoodsCategories(this.queryInfo)
+export default {
+  components: {
+    NavTitles,
+    MyCard,
+  },
+  data() {
+    return {
+      queryInfo: {
+        type: [], // 获取分类的级别
+        pagenum: 1, // 第几页
+        pagesize: 2, // 每页多少条
+      },
+      categories: [], // 商品分类列表
+      total: 0, // 商品总条数
+      columns: [
+        // 分类名称列
+        { label: "分类名称", prop: "cat_name" },
+        // 是否有效列
+        { label: "是否有效", type: "template", template: "isOk" },
+        // 排序列
+        { label: "排序", type: "template", template: "level" },
+        // 操作列
+        { label: "操作", type: "template", template: "operation" },
+      ],
+    };
+  },
+  methods: {
+    // 获取商品分类列表
+    async getCategories() {
+      const { data: res } = await requestGoodsCategories(this.queryInfo);
 
-        if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
 
-        this.categories = res.data.result
-        this.total = res.data.total
-        console.log(res);
-      }
+      this.categories = res.data.result;
+      this.total = res.data.total;
+      console.log(res);
     },
-    created () {
+    // 分页size发生变化
+    handleSizeChange(pagesize) {
+      this.queryInfo.pagenum = 1
+      this.queryInfo.pagesize = pagesize
       this.getCategories()
     },
-  }
+    // 页码发生变化 
+    handleCurrentChange(pagenum) {
+      this.queryInfo.pagenum = pagenum
+      this.getCategories()
+    }
+  },
+  created() {
+    this.getCategories();
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
+  .el-pagination {
+    margin-top: 15px;
+  }
 </style>
