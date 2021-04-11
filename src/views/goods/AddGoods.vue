e963ded93aa3e69c25f6dc4c01ff045502b0147f
DamonMok <damonmok1216@gmail.com>
2021-04-11 12:41:41 +0800

    步骤条和Tabs标签页联动

--- src/views/goods/AddGoods.vue
+++ src/views/goods/AddGoods.vue
@@ -9,7 +9,7 @@
       <el-alert title="添加商品信息" type="info" center show-icon :closable="false"></el-alert>
 
       <!-- 步骤区域 -->
-      <el-steps :space="200" :active="activeIndex " finish-status="success" align-center>
+      <el-steps :space="200" :active="activeIndex-0" finish-status="success" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -19,12 +19,12 @@
       </el-steps>
 
       <!-- Tabs标签页 -->
-      <el-tabs tab-position="left" style="height: 200px;">
-        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <el-tabs v-model="activeIndex" tab-position="left" style="height: 200px;">
+        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
       </el-tabs>
     </my-card>
   </div>
@@ -41,7 +41,7 @@ export default {
   },
   data() {
     return {
-      activeIndex: 0,
+      activeIndex: "0",
     };
   },
 };
