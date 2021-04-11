e9d8c6a0c7b37c25545cbcdaf52fd7b5fdf40e12
DamonMok <damonmok1216@gmail.com>
2021-04-11 12:28:20 +0800

    渲染步骤条

--- src/common/element.js
+++ src/common/element.js
@@ -3,7 +3,7 @@ import {
         Input, Form, FormItem, Button, Message, Container, Header, Aside, Main,
         Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem,
         Card, Row, Col, Table, TableColumn, Switch, Pagination,
-        Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane
+        Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step
 } from 'element-ui';
 
 Vue.use(Input)
@@ -35,6 +35,8 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
--- src/views/goods/AddGoods.vue
+++ src/views/goods/AddGoods.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
-    添加商品
+    <!-- 面包屑导航 -->
+    <nav-titles :titles="['商品管理', '添加商品']"></nav-titles>
+
+    <!-- 卡片区域 -->
+    <my-card>
+      <!-- 警告区域 -->
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false"></el-alert>
+
+      <!-- 步骤区域 -->
+      <el-steps :space="200" :active="activeIndex " finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+    </my-card>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import NavTitles from "components/content/NavTitles";
+import MyCard from "components/content/MyCard";
+
+export default {
+  components: {
+    NavTitles,
+    MyCard,
+  },
+  data() {
+    return {
+      activeIndex: 0,
+    };
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
+.el-steps {
+  margin-top: 15px;
+}
 </style>
